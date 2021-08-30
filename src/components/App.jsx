900342e55e2c5ed9dbf1a6e9fdcc090472499441
Alina Melnyk <alinkaaa345@gmail.com>
2021-08-31 02:29:37 +0300

    task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,15 @@ import user from 'data/user.json';
 import Profile from 'components/SocialProfile/SocialProfile';
 
 export default function App() {
-    return (
-      <div>
-        <Profile
-            name={user.name}
-            tag={user.tag}
-            location={user.location}
-            avatar={user.avatar}
-            stats={user.stats}
-        />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
